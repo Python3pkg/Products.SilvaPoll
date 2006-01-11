ee6618baa4f740af6a1ff3e9935d110e06534190
Guido Wesdorp <info@infrae.com>
2006-01-11 17:40:56 +0000

    Removed some unused crap.

--- poll/poll.py
+++
@@ -1,4 +0,0 @@
-class Poll:
-    def __init__(self, db):
-        self._db = db
-
--- poll/question.py
+++
@@ -1,8 +0,0 @@
-class Question:
-    def __init__(self, question, answers):
-        self.question = question
-        self.answers = answers
-        self.votes = len(answers) * [0]
-
-    def vote(self, index):
-        self.votes[index] += 1
